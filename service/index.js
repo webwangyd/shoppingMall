39c5a45745b4628a1601e71bbf9d85dee2dfa7bd
webwangyd <1131847259@qq.com>
2019-02-21 23:03:25 +0800

    mongoose链接数据库

---
+++ service/index.js
@@ -0,0 +1,13 @@
+const Koa = require('koa')
+const app = new Koa()
+const {connect} = require('./database/init.js')
+//立即执行函数
+;(async () =>{
+  await connect()
+})()
+app.use(async(ctx) => {
+  ctx.body = '<h1>hello webwangd</h1>'
+})
+app.listen(3000,()=>{
+  console.log('[Server] starting at port 3000')
+})
